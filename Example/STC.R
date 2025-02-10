cc6d3bc666486a45a10fb9195d189c4804bbba77
JiaRuofan <154356867+JiaRuofan@users.noreply.github.com>
2025-02-10 10:16:48 +0800

    Update STC.R

--- Example/STC.R
+++ Example/STC.R
@@ -1,5 +1,8 @@
-AB_IPD <- read.csv("AB_IPD.csv") # load AB patient-level data
-AC_AGD <- read.csv("AC_AGD.csv") # load AC aggregate-level data
+AB_IPD <- cbind(x_AB,t1_censored,delta1,c(rep(0,n_AB/2),rep(1,n_AB/2)))
+colnames(AB_IPD)=c('X1','X2','X3','X4','X5',t,delta,trt)
+
+AC_AGD <- apply(x_AC,2,mean)
+colnames(AC_AGD)=c('mean_X1','mean_X2','mean_X3','mean_X4','mean_X5')
 
 # fit regression of outcome on the baseline characteristics and treatment
 # effect modifiers are centered at the mean AC values
